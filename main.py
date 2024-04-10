0c4497cdaacdf825e48d547187866a30b69a9338
AbigailPoma <72541050@continental.edu.pe>
2024-04-10 18:23:53 -0500

    Codigo mejorado

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
-# Ingresa la calificación
-calificacion = float(input("Ingresa la calificación: "))
+def obtener_calificacion(nota):
+    if nota > 9.0:
+        return "A"
+    elif nota > 8.0:
+        return "B"
+    elif nota >= 7.5:
+        return "C"
+    else:
+        return "R"
 
-if(calificacion > 9.0):
-    print("La calificación es A.")
-elif(calificacion > 8.0):
-    print("La calificación es B.")
-elif(calificacion >= 7.5):
-    print("La calificación es C.")
-else:
-    print("La calificación es R.")
+try:
+    calificacion = float(input("Ingresa la calificación: "))
+    if calificacion < 0 or calificacion > 10:
+        print("Por favor, ingresa una calificación válida entre 0 y 10.")
+    else:
+        print(f"La calificación es {obtener_calificacion(calificacion)}.")
+except ValueError:
+    print("Por favor, ingresa un número.")
